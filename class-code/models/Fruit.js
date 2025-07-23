cd0d9e3a6721313cc4b569ed203862b9e25ee124
omarakamal <omaraakamal@gmail.com>
2025-07-23 13:20:17 +0300

    update

--- class-code/models/Fruit.js
+++ class-code/models/Fruit.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose")
+
+const fruitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true
+    },
+    isReadyToEat:{
+        type:Boolean,
+        Default:false
+    }
+},{timestamps:true})
